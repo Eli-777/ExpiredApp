ddfdc0f01589b78ad184e8383a53da0581476d47
Eli <jo458405@hotmail.com>
2021-07-15 13:58:23 +0800

    [api] set fallbackController to get angular html static file

--- API/Startup.cs
+++ API/Startup.cs
@@ -56,9 +56,13 @@ namespace API
             app.UseAuthentication();
             app.UseAuthorization();
 
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                endpoints.MapFallbackToController("Index", "Fallback");
             });
         }
     }
